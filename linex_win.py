beac751c29a16cf62c637b8cec159dbef5e5c961
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2022-02-25 15:49:29 -0600

    Create linex_win.py

---
+++ linex_win.py
@@ -0,0 +1,48 @@
+import json
+import requests
+import os
+import time
+
+logo = """▄▀▀▀▀▄     ▄▀▀█▀▄    ▄▀▀▄ ▀▄  ▄▀▀█▄▄▄▄  ▄▀▀▄  ▄▀▄ 
+█    █     █   █  █  █  █ █ █ ▐  ▄▀   ▐ █    █   █ 
+▐    █     ▐   █  ▐  ▐  █  ▀█   █▄▄▄▄▄  ▐     ▀▄▀  
+    █          █       █   █    █    ▌       ▄▀ █  
+  ▄▀▄▄▄▄▄▄▀ ▄▀▀▀▀▀▄  ▄▀   █    ▄▀▄▄▄▄       █  ▄▀  
+  █        █       █ █    ▐    █    ▐     ▄▀  ▄▀   
+  ▐        ▐       ▐ ▐         ▐         █    ▐   """
+
+
+
+def info():
+  os.system("cls")
+  os.system("color 1")
+  print(logo)
+  key = "d0f8d48fb6cccba08753c1ed51fffae5"
+  print('[~] Ejemplo: 14158586273')
+  numero = input('[~] Ingresa el numero de telefono: ')
+  api = f"http://apilayer.net/api/validate?access_key={key}&number={numero}"
+  data = requests.get(api).json()
+  print('\n[~] Numero: ', data['number'])
+  print('[~] Codigo del pais: ', data['country_code'])
+  print('[~] Nombre del pais: ', data['country_name'])
+  print('[~] Ubicacion: ', data['location'])
+  print('[~] Transportador: ', data['carrier'])
+  
+
+def menu():
+  os.system("cls")
+  os.system("color 1")
+  print(logo)
+  print('\n[1] Buscar informacion de un numero')
+  print('[99] Salir')
+  T = int(input('\n>> '))
+  if T == 1:
+    info()
+  elif T == 99:
+    exit()
+  else:
+    print('Error.')
+    time.sleep(1)
+    menu()
+
+menu()
